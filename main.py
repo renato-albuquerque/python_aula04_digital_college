d8514cb28b43f8c072256b42c3a48fcd2095ba68
renato_albuquerque <renatomalb@hotmail.com>
2025-05-12 23:05:27 -0300

    using ydata_profiling

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import numpy
+import pandas as pd
+from ydata_profiling import ProfileReport
 
-print("Hello world!")
+df = pd.read_csv('base_vendas.csv', encoding='latin1')
+profile = ProfileReport(df, title="Profiling Report")
+profile.to_file("your_report.html")
